ccc0691942ac64f0a0a2b0ca1e35503673ab6623
HesterSho <lolittasisi123@gmail.com>
2025-09-23 23:18:02 +0100

    add first components

---
+++ src/Accueil/Accueil.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Categories from './Categories'
+import Footer from '../Navbar-Footer/Footer'
+
+export default function Accueil() {
+  return (
+    <div>
+       
+      <Categories/>
+       <Footer/> 
+    </div>
+  )
+}
---
+++ src/Navbar-Footer/Footer.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import "./Nav-Footer.css";
+import { Link } from 'react-router-dom';
+
+export default function Footer() {
+  return (
+    <footer className="container2">
+            <div className="footer-content">
+                <div className="footer-section">
+                    <h3>Réseaux sociaux</h3>
+                    <div className="social-icons">
+                        <div className='icons-media'> <i class="fab fa-facebook"></i><p to="/" className='a'>Facebook</p></div>
+                        <div className='icons-media'> <i class="fab fa-twitter"></i>
+                        <p>Twitter</p>
+                    </div>    
+                    <div className='icons-media'><i class="fab fa-linkedin"></i><p>Linkedin</p></div>
+                    <div className='icons-media'><i class="fab fa-instagram"></i><p>Instagram</p></div>
+                    </div>
+                </div>
+                 <div class="footer-section">
+                    <h3>Liens rapides</h3>
+                    <ul>
+                        <li><Link to="/" className='a'>Accueil</Link></li>
+                        <li><Link to="/"className='a'>A propos</Link></li>
+                        <li><Link to="/"className='a'>Publier</Link></li>
+                        <li><Link to="/"className='a'>Favoris</Link></li>
+                        <li><Link to="/"className='a'>Admin</Link></li>
+                    </ul>
+                </div>
+                 <div class="footer-section">
+                    <h3>Support</h3>
+                    <ul>
+                        <li><Link to="/" className='a'>Centre d'aide</Link></li>
+                        <li><Link to="/" className='a'>Guide utilisateur</Link></li>
+                        <li><Link to="/"className='a'>Contacter le support</Link></li>
+                        <li><Link to="/" className='a'>Signaler un problème</Link></li>
+                    </ul>
+                </div>
+                 <div class="footer-section">
+                    <h3>Informations de contact</h3>
+                    <div className='icons-media'><i class="fas fa-map-marker-alt"></i>
+                    <p>Boulevard Abderrahim, Casablanca.</p></div>
+                    <div className='icons-media'><i class="fas fa-phone"></i>
+                    <p>+212 650678910</p></div>
+                    <div className='icons-media'><i class="fas fa-envelope"></i>
+                             <p>info@artconnect.com</p></div>
+                    
+                    
+           
+                </div>
+            </div>
+            <div class="footer-bottom">
+          <p>
+        &copy; 2025 ARTCONNECT & MAROC. All rights reserved. | <Link to="#">Privacy Policy</Link> |
+            <Link to="#">Terms of Service</Link>
+          </p>
+         </div>
+    </footer>
+  )
+}
